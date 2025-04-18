0b3658f4f711c7604323206d9ad8b20fee8298f6
farchettiensis <110430578+farchettiensis@users.noreply.github.com>
2025-04-18 00:20:42 -0300

    courseinfo (1.5): wrap all variables under one single object

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,8 +1,8 @@
-const Header = ({ course }) => (
-  <h1>{course}</h1>
+const Header = ({ course: { name } }) => (
+  <h1>{name}</h1>
 );
 
-const Content = ({ parts }) => {
+const Content = ({ course: { parts } }) => {
   const [part1, part2, part3] = parts;
 
   return (
@@ -18,7 +18,7 @@ const Part = ({ name, exercises }) => (
   <p>{name} {exercises}</p>
 );
 
-const Total = ({ parts }) => {
+const Total = ({ course: { parts } }) => {
   const total = parts.reduce((sum, part) => sum + part.exercises, 0);
 
   return (
@@ -27,18 +27,20 @@ const Total = ({ parts }) => {
 };
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const parts = [
-    { name: 'Fundamentals of React', exercises: 10 },
-    { name: 'Using props to pass data', exercises: 7 },
-    { name: 'State of a component', exercises: 14 }
-  ];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      { name: 'Fundamentals of React', exercises: 10 },
+      { name: 'Using props to pass data', exercises: 7 },
+      { name: 'State of a component', exercises: 14 }
+    ]
+  };
 
   return (
     <div>
       <Header course={course}/>
-      <Content parts={parts}/>
-      <Total parts={parts}/>
+      <Content course={course}/>
+      <Total course={course}/>
     </div>
   );
 };
