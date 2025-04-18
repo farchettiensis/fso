9c8a9aab24746917496569dcaac53133e83fe7fe
farchettiensis <110430578+farchettiensis@users.noreply.github.com>
2025-04-17 23:54:59 -0300

    courseinfo (1.4): place the objects into an array and refactor accordingly

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -2,47 +2,43 @@ const Header = ({ course }) => (
   <h1>{course}</h1>
 );
 
-const Content = ({ part1, part2, part3 }) => (
-  <>
-    <Part name={part1.name} exercises={part1.exercises}></Part>
-    <Part name={part2.name} exercises={part2.exercises}></Part>
-    <Part name={part3.name} exercises={part3.exercises}></Part>
-  </>
-);
+const Content = ({ parts }) => {
+  const [part1, part2, part3] = parts;
+
+  return (
+    <div>
+      <Part name={part1.name} exercises={part1.exercises}/>
+      <Part name={part2.name} exercises={part2.exercises}/>
+      <Part name={part3.name} exercises={part3.exercises}/>
+    </div>
+  );
+};
 
 const Part = ({ name, exercises }) => (
   <p>{name} {exercises}</p>
 );
 
-const Total = ({ total }) => (
-  <p>Number of exercises {total}</p>
-);
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+
+  return (
+    <p>Number of exercises {total}</p>
+  );
+};
 
 const App = () => {
   const course = 'Half Stack application development';
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  };
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  };
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  };
-  const total = part1.exercises + part2.exercises + part3.exercises;
+  const parts = [
+    { name: 'Fundamentals of React', exercises: 10 },
+    { name: 'Using props to pass data', exercises: 7 },
+    { name: 'State of a component', exercises: 14 }
+  ];
 
   return (
     <div>
       <Header course={course}/>
-      <Content
-        part1={part1}
-        part2={part2}
-        part3={part3}
-      />
-      <Total total={total}/>
+      <Content parts={parts}/>
+      <Total parts={parts}/>
     </div>
   );
 };
