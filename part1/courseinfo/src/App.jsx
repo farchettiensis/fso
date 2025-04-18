c3ca2ad97b7a61affe3b91bb981ea7ea038c5586
farchettiensis <110430578+farchettiensis@users.noreply.github.com>
2025-04-17 23:27:17 -0300

    refactor (1.3): wrap the parts' names and exercises in objects

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -2,22 +2,16 @@ const Header = ({ course }) => (
   <h1>{course}</h1>
 );
 
-const Content = (
-  {
-    part1, exercises1,
-    part2, exercises2,
-    part3, exercises3
-  }
-) => (
+const Content = ({ part1, part2, part3 }) => (
   <>
-    <Part part={part1} exercises={exercises1}></Part>
-    <Part part={part2} exercises={exercises2}></Part>
-    <Part part={part3} exercises={exercises3}></Part>
+    <Part name={part1.name} exercises={part1.exercises}></Part>
+    <Part name={part2.name} exercises={part2.exercises}></Part>
+    <Part name={part3.name} exercises={part3.exercises}></Part>
   </>
 );
 
-const Part = ({ part, exercises }) => (
-  <p>{part} {exercises}</p>
+const Part = ({ name, exercises }) => (
+  <p>{name} {exercises}</p>
 );
 
 const Total = ({ total }) => (
@@ -26,21 +20,27 @@ const Total = ({ total }) => (
 
 const App = () => {
   const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
-  const total = exercises1 + exercises2 + exercises3;
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  };
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  };
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  };
+  const total = part1.exercises + part2.exercises + part3.exercises;
 
   return (
     <div>
       <Header course={course}/>
       <Content
-        part1={part1} exercises1={exercises1}
-        part2={part2} exercises2={exercises2}
-        part3={part3} exercises3={exercises3}
+        part1={part1}
+        part2={part2}
+        part3={part3}
       />
       <Total total={total}/>
     </div>
